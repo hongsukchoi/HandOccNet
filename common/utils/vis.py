87a9d211f46efdd96d6fd943af152dc7d6b43238
hongsuk.c <hongsuk.c@samsung.com>
2023-09-19 15:49:35 -0400

    remove pyrender

--- common/utils/vis.py
+++ common/utils/vis.py
@@ -4,8 +4,6 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-os.environ["PYOPENGL_PLATFORM"] = "egl"
-import pyrender
 import trimesh
 
 def vis_keypoints_with_skeleton(img, kps, kps_lines, kp_thresh=0.4, alpha=1):
@@ -121,38 +119,4 @@ def save_obj(v, f, file_name='output.obj'):
     obj_file.close()
 
 def render_mesh(img, mesh, face, cam_param):
-    # mesh
-    mesh = trimesh.Trimesh(mesh, face)
-    rot = trimesh.transformations.rotation_matrix(
-	np.radians(180), [1, 0, 0])
-    mesh.apply_transform(rot)
-    material = pyrender.MetallicRoughnessMaterial(metallicFactor=0.0, alphaMode='OPAQUE', baseColorFactor=(1.0, 1.0, 0.9, 1.0))
-    mesh = pyrender.Mesh.from_trimesh(mesh, material=material, smooth=False)
-    scene = pyrender.Scene(ambient_light=(0.3, 0.3, 0.3))
-    scene.add(mesh, 'mesh')
-    
-    focal, princpt = cam_param['focal'], cam_param['princpt']
-    camera = pyrender.IntrinsicsCamera(fx=focal[0], fy=focal[1], cx=princpt[0], cy=princpt[1])
-    scene.add(camera)
- 
-    # renderer
-    renderer = pyrender.OffscreenRenderer(viewport_width=img.shape[1], viewport_height=img.shape[0], point_size=1.0)
-   
-    # light
-    light = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=0.8)
-    light_pose = np.eye(4)
-    light_pose[:3, 3] = np.array([0, -1, 1])
-    scene.add(light, pose=light_pose)
-    light_pose[:3, 3] = np.array([0, 1, 1])
-    scene.add(light, pose=light_pose)
-    light_pose[:3, 3] = np.array([1, 1, 2])
-    scene.add(light, pose=light_pose)
-
-    # render
-    rgb, depth = renderer.render(scene, flags=pyrender.RenderFlags.RGBA)
-    rgb = rgb[:,:,:3].astype(np.float32)
-    valid_mask = (depth > 0)[:,:,None]
-
-    # save to image
-    img = rgb * valid_mask*0.5 + img #* (1-valid_mask)
-    return img
+    pass
